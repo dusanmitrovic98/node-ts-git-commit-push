12af2f844e74f85e8a650c8381d6150dad6552f4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-05 10:09:28 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Copyright (c) 2023 Dusan Mitrovic (BK2O198)
 
+import dotenv from "dotenv";
+
 import commitAndPush from "./modules/git-commit-push.js";
+
+dotenv.config();
 
 const pathRepository: string = "";
 const commitMessage: string = "";

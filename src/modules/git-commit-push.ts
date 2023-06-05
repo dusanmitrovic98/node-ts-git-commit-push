41f6a92c2008da745f278e28b5111a5d63415afd
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-05 10:08:57 +0200

    Update git-commit-push.ts

--- src/modules/git-commit-push.ts
+++ src/modules/git-commit-push.ts
@@ -4,6 +4,10 @@ async function commitAndPush(
   pathRepository: string,
   commitMessage: string
 ): Promise<void> {
+  if (!Validate(pathRepository, commitMessage)) {
+    return;
+  }
+
   const git = simpleGit(pathRepository);
 
   await git.add("./*");
@@ -11,6 +15,20 @@ async function commitAndPush(
   await git.push();
 
   console.log("Changes committed and pushed successfully.");
+}
+
+function Validate(pathRepository: string, commitMessage: string): boolean {
+  if (pathRepository == "" || pathRepository == undefined) {
+    console.log("Repository path not specified.");
+    return false;
+  }
+
+  if (commitMessage == "" || commitMessage == undefined) {
+    console.log("Commit message not specified.");
+    return false;
+  }
+
+  return true;
 }
 
 export default commitAndPush;

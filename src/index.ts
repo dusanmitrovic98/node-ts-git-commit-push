8e20692ac7de3b6a478ebf8cab5b6b721e82a93d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-05 16:41:57 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 import commitAndPush, {
   addExceptionForDirectory,
-} from "./modules/git-commit-push/git-commit-push.js";
+} from "./modules/git-commit-push.js";
 import prompt from "./utility/prompt/prompt.js";
 
 dotenv.config();
